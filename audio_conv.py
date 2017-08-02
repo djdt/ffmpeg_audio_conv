7e2321bfccd5506fccf3b4bc553459573a9a1df5
djdt <djdt@djdt.djdt>
2017-08-02 21:46:01 +1000

    move stuff around

--- audio_conv.py
+++ audio_conv.py
@@ -1,49 +1,39 @@
+#!/usr/bin/python3
 import argparse
 import logging
 import os
-import subprocess
+import signal
 import sys
 import time
 
+import multiline
+from converter import Converter
 
-def find_files(base_dir, exts):
+
+class CleanupKiller:
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit)
+        signal.signal(signal.SIGTERM, self.exit)
+
+    def exit(self, signum, frame):
+        self.kill_now = True
+
+
+def find_files(base_dir, exts, recurse=False):
     matches = []
-    for files in os.listdir(base_dir):
-        f = os.path.join(base_dir, files)
-        if os.path.isfile(f) and os.path.splitext(f)[1][1:] in exts:
-                matches.append(f)
+    if recurse:
+        for root, dirs, files in os.walk(base_dir):
+            for f in files:
+                if os.path.splitext(f)[1][1:] in exts:
+                    matches.append(os.path.join(root, f))
+    else:
+        for files in os.listdir(base_dir):
+            f = os.path.join(base_dir, files)
+            if os.path.isfile(f) and os.path.splitext(f)[1][1:] in exts:
+                    matches.append(f)
     return matches
-
-
-def find_all_files(base_dir, exts):
-    matches = []
-    for root, dirs, files in os.walk(base_dir):
-        for f in files:
-            if os.path.splitext(f)[1][1:] in exts:
-                matches.append(os.path.join(root, f))
-    return matches
-
-
-def progress_display(count, total, msg, threads):
-    bar_len = 32
-    filled_len = int(round(bar_len * count) / float(total))
-
-    percent = count / float(total)
-    bar = '#' * filled_len + ' ' * (bar_len - filled_len)
-
-    line2 = '[{}] {:>6.1%}'.format(bar, percent)
-
-    # Print one line per thread
-    for m in msg:
-        print('\033[K', end='')
-        print('Current file:', m)
-    for i in range(threads - len(msg)):
-        print('\033[K', end='')
-        print('Finished.')
-    # Print progress bar
-    print('\033[K', end='')
-    print(line2)
-    print('\033[F' * (threads + 1), end='')
 
 
 def get_output_file_path(in_path, in_base, out_base, new_ext):
@@ -52,121 +42,131 @@ def get_output_file_path(in_path, in_base, out_base, new_ext):
     return os.path.join(out_base, path)
 
 
+def progress_display(count, total, elapsed_time, msg, threads):
+    bar_len = 32
+    filled_len = int(round(bar_len * count) / float(total))
+
+    bar = '#' * filled_len + ' ' * (bar_len - filled_len)
+    bar_line = '[{}] {:>6.1%} {}'.format(
+            bar, count / float(total),
+            time.strftime('%H:%M:%S', time.gmtime(
+                elapsed_time * (total - (total - count)))))
+
+    lines = []
+    for m in msg:
+        lines.append('Current file: ' + m)
+    for i in range(len(msg), threads):
+        lines.append("Finished.")
+    lines.append(bar_line)
+
+    multiline.print_lines(lines)
+
+
 def parse_args(args):
     parser = argparse.ArgumentParser(
         description='Converts audio files using ffmpeg.')
     parser.add_argument('indir', help='Input directory.')
     parser.add_argument('outdir', help='Output directory.')
-    parser.add_argument('-i', '--informat', default=None, nargs='*',
+    parser.add_argument('-i', '--informat', type=str, nargs='*',
                         help='Format(s) to convert (use ext(s)).')
-    parser.add_argument('-o', '--outformat', default=None,
+    parser.add_argument('-o', '--outformat', type=str,
                         help='Target format (use ext.).')
     parser.add_argument('-r', '--recurse', action='store_true',
                         help='Recurse the input directory.')
-    parser.add_argument('-q', '--quality', type=str, default=None,
+    parser.add_argument('-q', '--quality', type=str,
                         help='Quality to pass to ffmpeg.')
-    parser.add_argument('-b', '--bitrate', type=str, default=None,
+    parser.add_argument('-b', '--bitrate', type=str,
                         help='Target bitrate, e.g. 320k.')
     parser.add_argument('-t', '--threads', type=int, default=4,
                         help='Maximum number of threads used.')
     return vars(parser.parse_args(args))
 
 
-def main():
-    start = time.time()
-    args = parse_args(sys.argv[1:])
-    if args['quality'] is not None and args['bitrate'] is not None:
-        print('Cannot define quality AND bitrate, use one.')
-        sys.exit(1)
-
+def setup_logger():
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     handler = logging.FileHandler('audio_conv.log', 'w')
     handler.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
     logger.addHandler(handler)
     logger.propagate = False
+    return logger
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+    if args['quality'] is not None:
+        options = ['-q', args['quality']]
+    elif args['bitrate'] is not None:
+        options = ['-b', args['bitrate']]
+    else:
+        options = []
+
+    logger = setup_logger()
+    killer = CleanupKiller()
 
     source_path = os.path.abspath(os.path.expanduser(args['indir']))
     dest_path = os.path.abspath(os.path.expanduser(args['outdir']))
 
     # Gather input files
-    source_files = []
-    if args['recurse']:
-        source_files = find_all_files(source_path, args['informat'])
-    else:
-        source_files = find_files(source_path, args['informat'])
+    source_files = find_files(source_path,
+                              args['informat'], args['recurse'])
 
     # Sort reverse to allow for pop
     source_files.sort(reverse=True)
 
-    procs = []
+    converter = Converter(options, args['threads'])
     prog_total = len(source_files)
-    prog_complete, prog_error, prog_skipped = 0, 0, 0
+    prog_skipped = 0
     update_progress = True
 
-    while prog_complete + prog_error + prog_skipped < prog_total:
-        # Check current processes
-        for proc, fn in procs:
-            ret = proc.poll()
-            if ret is not None:
-                # If error, print and exit
-                if ret > 0:
-                    print("Logged error for file:", fn)
-                    for line in proc.stderr:
-                        logger.info(line)
-                    prog_error += 1
-                else:
-                    prog_complete += 1
-                procs.remove((proc, fn))
-                update_progress = True
-                break
+    start_time = time.time()
+    elapsed_time = 0
 
-        if len(procs) < args['threads'] and len(source_files) > 0:
-            in_file = source_files.pop()
+    while converter.num_converted() + prog_skipped < prog_total:
+        # Check current processes
+        if converter.check_processes() > 0:
+            converter.log_errors(logger)
+            # update_progress = True
+
+        if converter.can_add_process() and len(source_files) > 0:
+            infile = source_files.pop()
             # Remove source_path, change ext, prepend dest_path
-            out_file = get_output_file_path(
-                    in_file, source_path, dest_path, args['outformat'])
+            outfile = get_output_file_path(
+                    infile, source_path, dest_path, args['outformat'])
             # If the file already exists, skip it
-            if os.path.exists(out_file):
+            if os.path.exists(outfile):
                 prog_skipped += 1
             else:
                 # Make new dir if needed
-                dir_name = os.path.dirname(out_file)
+                dir_name = os.path.dirname(outfile)
                 if not os.path.exists(dir_name):
                     os.makedirs(dir_name, exist_ok=True)
                     print('Creating dir:', dir_name)
-                # Build ffmpeg cmd
-                cmd = ['ffmpeg', '-i', in_file]
-                if args['quality'] is not None:
-                    cmd.extend(['-q', args['quality']])
-                elif args['bitrate'] is not None:
-                    cmd.extend(['-b', args['bitrate']])
-                cmd.append(out_file)
                 # Add new process to processes, store filename for print
-                p = subprocess.Popen(cmd, stdout=subprocess.DEVNULL,
-                                     stderr=subprocess.PIPE)
-                procs.append((p, os.path.basename(out_file)))
+                converter.new_process(infile, outfile)
             update_progress = True
 
         # Update the progress
+        elapsed_time = time.time() - start_time
         if update_progress:
-            progress_display(prog_complete + prog_error + prog_skipped,
-                             prog_total, [x[1] for x in procs],
-                             args['threads'])
+            progress_display(converter.num_converted() + prog_skipped,
+                             prog_total, elapsed_time, converter.status())
             update_progress = False
-        time.sleep(0.01)
+        else:
+            time.sleep(0.01)
+        if killer.kill_now:
+            converter.kill_all()
+            multiline.skip_lines(args['threads'] + 1)
+            print("Exiting...")
+            exit(1)
 
     # Display end msg
-    finish = time.time()
-    progress_display(1, 1, [], args['threads'])
-    print('\n' * args['threads'])
-    if prog_error > 0:
-        print('{} errors.'.format(prog_error))
-    if prog_skipped > 0:
-        print('{} files skipped.'.format(prog_skipped))
-    if prog_complete > 0:
-        print('{} files converted in {:.2f} seconds.'.format(
-            prog_complete, finish - start))
+    # progress_display(1, 1, 0, [], args['threads'])
+    # multiline.skip_lines(args['threads'] + 1)
+    print('Processed {} files in {:.2f} seconds.'.format(
+        converter.num_converted(), elapsed_time))
+    print('{} errors, {} skipped, {} converted.'.format(
+        converter.failed, prog_skipped, converter.completed))
 
     # Remove empty logs
     logging.shutdown()
---
+++ converter.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import signal
+
+
+class ConverterProcess:
+
+    def __init__(self, cmd, name):
+        self.name = name
+        self.process = subprocess.Popen(
+            cmd, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+
+    def kill(self):
+        self.process.send_signal(signal.SIGINT)
+        self.process.wait()
+
+    def poll(self):
+        return self.process.poll()
+
+    def log_error(self, logger):
+        for l in self.process.stderr:
+            logger.info(l)
+
+
+class Converter:
+
+    def __init__(self, options=[], threads=4):
+        self.options = options
+        self.threads = threads
+
+        self.processes = []
+        self.completed = 0
+        self.failed = 0
+
+    def check_processes(self):
+        ret = 0
+        for proc in self.processes:
+            status = proc.poll()
+            if status is not None:
+                if status > 0:
+                    self.failed += 1
+                    self.failed_processes.append(proc)
+                    ret += 1
+                else:
+                    self.completed += 1
+                self.processes.remove(proc)
+        return ret
+
+    def new_process(self, inpath, outpath):
+        cmd = ['ffmpeg', '-i', inpath]
+        cmd.extend(self.options)
+        cmd.append(outpath)
+        self.processes.append(
+            ConverterProcess(cmd, os.path.basename(inpath)))
+
+    def log_errors(self, logger):
+        for proc in self.failed_processes:
+            proc.log_error(logger)
+        self.failed_processes = []
+
+    def kill_all(self):
+        for proc in self.processes:
+            proc.kill()
+        self.processes = []
+
+    def num_processess(self):
+        return len(self.processes)
+
+    def can_add_process(self):
+        return self.num_processess() < self.threads
+
+    def process_names(self):
+        return [p.name for p in self.processes]
+
+    def num_converted(self):
+        return self.completed + self.failed
