31a15e9503acf0ce5822c31d590779bb03d4a588
djdt <djdt@djdt.djdt>
2017-08-02 11:03:29 +1000

    Comments and bitrate

--- audio_conv.py
+++ audio_conv.py
@@ -38,8 +38,6 @@ def progress_display(count, total, msg, end='\r'):
     print(line2, end='')
     print('\b' * len(line2), end='')
     print('\033[F', end='')
-    # print('[{}] {:>6.1%} [{}]{}'.format(
-    #     bar, percent, msg[:32], fill), end=end)
 
 
 def get_output_file_path(in_path, in_base, out_base, new_ext):
@@ -61,6 +59,8 @@ def parse_args(args):
                         help='Recurse the input directory.')
     parser.add_argument('-q', '--quality', type=str, default=None,
                         help='Quality to pass to ffmpeg.')
+    parser.add_argument('-b', '--bitrate', type=str, default=None,
+                        help='Target bitrate, e.g. 320k.')
     parser.add_argument('-t', '--threads', type=int, default=4,
                         help='Maximum number of threads used.')
     return vars(parser.parse_args(args))
@@ -69,16 +69,21 @@ def parse_args(args):
 def main():
     start = time.time()
     args = parse_args(sys.argv[1:])
-    procs = []
+    if args['quality'] is not None and args['bitrate'] is not None:
+        print('Cannot define quality AND bitrate, use one.')
+        sys.exit(1)
+
     source_path = os.path.abspath(os.path.expanduser(args['indir']))
     dest_path = os.path.abspath(os.path.expanduser(args['outdir']))
 
+    # Gather input files
     source_files = []
     if args['recurse']:
         source_files = find_all_files(source_path, args['informat'])
     else:
         source_files = find_files(source_path, args['informat'])
 
+    procs = []
     prog_total = len(source_files)
     prog_complete = 0
     update_progress = True
@@ -88,13 +93,14 @@ def main():
         for proc, fn in procs:
             ret = proc.poll()
             if ret is not None:
+                # If error, print and exit
                 if ret > 0:
                     print("ffmpeg error: ")
                     for line in proc.stderr:
                         print(line)
                     for p in procs:
                         p.kill
-                    return
+                    sys.exit(1)
                 procs.remove((proc, fn))
                 prog_complete += 1
                 update_progress = True
@@ -105,11 +111,16 @@ def main():
             # Remove source_path, change ext, prepend dest_path
             out_file = get_output_file_path(
                     in_file, source_path, dest_path, args['outformat'])
+            # If the file already exists, skip it
             if not os.path.exists(out_file):
+                # Build ffmpeg cmd
                 cmd = ['ffmpeg', '-i', in_file]
                 if args['quality'] is not None:
                     cmd.extend(['-q', args['quality']])
+                elif args['bitrate'] is not None:
+                    cmd.extend(['-b', args['bitrate']])
                 cmd.append(out_file)
+                # Add new process to processes, store filename for print
                 p = subprocess.Popen(cmd, stdout=subprocess.DEVNULL,
                                      stderr=subprocess.PIPE)
                 procs.append((p, os.path.basename(out_file)))
@@ -117,11 +128,13 @@ def main():
                 prog_complete += 1
             update_progress = True
 
+        # Update the progress
         if update_progress:
             progress_display(prog_complete, prog_total, [x[1] for x in procs])
             update_progress = False
         time.sleep(0.01)
 
+    # Display end msg
     finish = time.time()
     progress_display(1, 1, [])
     print('\n')
