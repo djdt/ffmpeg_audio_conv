662285c6ee47057511b2ab4577498e37e4d204b3
djdt <djdt@gc-linux>
2017-08-03 20:06:47 +1000

    added return of misc files

--- util/fileops.py
+++ util/fileops.py
@@ -5,30 +5,29 @@ def expand_path(path):
     return os.path.abspath(os.path.expanduser(path))
 
 
-def create_dirs_if_not_exists(dir):
-    dir = expand_path(dir)
-    if not os.path.exists(dir):
-        os.makedirs(dir, exist_ok=True)
-        return True
-    return False
-
-
-def gather_files(dir, exts=None, recurse=False):
+def gather_files(dir, exts=None, recurse=False, ignore_other=False):
     dir = expand_path(dir)
     matches = []
+    other_files = []
     if recurse:
         for root, dirs, files in os.walk(dir):
             for f in files:
                 if exts is None or os.path.splitext(f)[1][1:] in exts:
                     matches.append(os.path.join(root, f))
+                elif not ignore_other:
+                    other_files.append(os.path.join(root, f))
     else:
         for files in os.listdir(dir):
             f = os.path.join(dir, files)
             if os.path.isfile(f):
                 if exts is None or os.path.splitext(f)[1][1:] in exts:
                     matches.append(f)
+                elif not ignore_other:
+                    other_files.append(f)
+
     matches.sort(reverse=True)
-    return matches
+    other_files.sort(reverse=True)
+    return matches, other_files
 
 
 def convert_path(file, indir, outdir, ext):
